85a49515743aad3adc4a3dd9ac7c0c3b730a7c90
Luke Chandler <lhchandler4@gmail.com>
2019-06-12 11:12:41 -0700

    Initial commit from Spring init

---
+++ src/main/java/com/LukeLab11/WhatIsGoingOn/songr/SongrApplication.java
@@ -0,0 +1,13 @@
+package com.LukeLab11.WhatIsGoingOn.songr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrApplication.class, args);
+	}
+
+}
