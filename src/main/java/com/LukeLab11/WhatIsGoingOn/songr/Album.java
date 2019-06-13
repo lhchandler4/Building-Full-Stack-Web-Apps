421c0239645c305c5b27e9c3e704a93053d4cc42
Luke Chandler <lhchandler4@gmail.com>
2019-06-13 12:46:48 -0700

    finished album lab

--- src/main/java/com/LukeLab11/WhatIsGoingOn/songr/Album.java
+++ src/main/java/com/LukeLab11/WhatIsGoingOn/songr/Album.java
@@ -1,4 +1,53 @@
 package com.LukeLab11.WhatIsGoingOn.songr;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String title;
+    String artist;
+    int songCount;
+    int length;
+    String imageUrl;
+
+    public Album() {}
+    public Album(String title, String artist, int songCount, int length, String imageUrl) {
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getArtist() {
+        return this.artist;
+    }
+
+    public int getSongCount() {
+        return this.songCount;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public String getImageUrl() {
+        return this.imageUrl;
+    }
 }
+
